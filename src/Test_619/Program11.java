5f82defec6493b529d5c7b759f69fe9e312255b7
Nancy Sharma <nancsharma@deloitte.com>
2025-06-19 14:57:16 +0530

    file is added

---
+++ src/Test_619/Program11.java
@@ -0,0 +1,15 @@
+package Test_619;
+
+import java.util.Scanner;
+
+public class Program11 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Print number");
+        int a=sc.nextInt();
+
+        for(int i=1;i<=a;i++){
+            System.out.print(i);
+        }
+    }
+}
