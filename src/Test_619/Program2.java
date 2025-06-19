ec54e9d5f66ed543d49200a659621b97d49b30ef
Nancy Sharma <nancsharma@deloitte.com>
2025-06-19 14:08:53 +0530

    file is added

---
+++ src/Test_619/Program2.java
@@ -0,0 +1,20 @@
+package Test_619;
+
+
+
+public class Program2 {
+    public static void main(String[] args) {
+        int A=10; int B=5;
+         int c=A + B;
+        System.out.println("Addition :"+c);
+        int d=A - B;
+        System.out.println("Subtraction :"+d);
+        int e=A * B;
+        System.out.println("Multiplication :"+e);
+        int f=A / B;
+        System.out.println("Division :"+f);
+        int g=A%B;
+        System.out.println("Modulus :"+g);
+
+    }
+}
