227794e57b0eee44cc212ef3865f2e80a494c4b5
Nancy Sharma <nancsharma@deloitte.com>
2025-06-26 15:47:26 +0530

    file is added

---
+++ src/June26/Program1.java
@@ -0,0 +1,12 @@
+package June26;
+
+public class Program1 {
+    public static void main(String[] args) {
+     String s="Hello World";
+     //Length: 11, First char: H, Substring: World
+
+        System.out.println("Length:"+s.length());
+        System.out.println("First char:"+s.charAt(0));
+        System.out.println("Substring :"+s.substring(6,11));
+    }
+}
